54dd675ebe82c75b3dd0020930c8de08ee941c7e
AhmedAshrafMarzouk <15006@stemegypt.edu.eg>
2019-02-06 00:43:53 +0200

    Add files via upload

---
+++ 22.bat
@@ -0,0 +1,15 @@
+@echo [off]
+bx login
+Pause
+@echo [off]
+bx target -o 15006@stemegypt.edu.eg -s dev
+Pause
+@echo [off]
+cd C:\school 2\! Important\18 Extra\IBM Cloud\Hello
+Pause
+@echo [off]
+dir 
+Pause
+@echo [off]
+bx app push  
+Pause
